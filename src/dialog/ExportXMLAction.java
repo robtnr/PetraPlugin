b0f729b0eb3b8f902b3e01a6be63847958a7fb27
robtnr <roberto.cont94@gmail.com>
2017-10-06 17:22:35 +0200

    Modifiche nell'export dell'xml

--- src/dialog/ExportXMLAction.java
+++ src/dialog/ExportXMLAction.java
@@ -1,5 +1,8 @@
 package dialog;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NonNls;
@@ -44,30 +47,36 @@ public class ExportXMLAction extends AnAction {
             }
         });
         chooser.showOpenDialog(null);
-        File selectedFile = chooser.getSelectedFile();
-        String fileName = selectedFile.getAbsolutePath();
-        final String extension = ".xml";
-        if (!fileName.endsWith(extension)) {
-            fileName += extension;
-        }
-        File file = new File(fileName);
-        GregorianCalendar date = new GregorianCalendar();
-        String data = date.get(Calendar.DAY_OF_MONTH)+"/"+date.get(Calendar.MONTH)+"/"+date.get(Calendar.YEAR)+" "+date.get(Calendar.HOUR)+":"+date.get(Calendar.MINUTE)+":"+date.get(Calendar.SECOND);
-        @NonNls final PrintWriter writer;
-        try {
-            writer = new PrintWriter(new FileOutputStream(fileName));
-            writer.println("<PETRA date='"+data+"'>");
-            for (int i=0;i<averagedConsumptionsDataList.size();i++)
-            {
-                writer.println("\t<SIGNATURE name='"+averagedConsumptionsDataList.get(i).getSignature()+"'>");
-                writer.println("\t\t<JOULE>"+averagedConsumptionsDataList.get(i).getJoule()+"</JOULE>");
-                writer.println("\t\t<SECONDS>"+averagedConsumptionsDataList.get(i).getSeconds()+"</SECONDS>");
-                writer.println("\t</SIGNATURE>");
+        if(chooser.getSelectedFile()!=null) {
+            File selectedFile = chooser.getSelectedFile();
+            String fileName = selectedFile.getAbsolutePath();
+            final String extension = ".xml";
+            if (!fileName.endsWith(extension)) {
+                fileName += extension;
             }
-            writer.println("</PETRA>");
-            writer.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            File file = new File(fileName);
+            GregorianCalendar date = new GregorianCalendar();
+            String data = date.get(Calendar.DAY_OF_MONTH) + "/" + date.get(Calendar.MONTH) + "/" + date.get(Calendar.YEAR) + " " + date.get(Calendar.HOUR) + ":" + date.get(Calendar.MINUTE) + ":" + date.get(Calendar.SECOND);
+            @NonNls final PrintWriter writer;
+            try {
+                writer = new PrintWriter(new FileOutputStream(fileName));
+                writer.println("<PETRA date='" + data + "'>");
+                for (int i = 0; i < averagedConsumptionsDataList.size(); i++) {
+                    writer.println("\t<SIGNATURE name='" + averagedConsumptionsDataList.get(i).getSignature() + "'>");
+                    writer.println("\t\t<JOULE>" + averagedConsumptionsDataList.get(i).getJoule() + "</JOULE>");
+                    writer.println("\t\t<SECONDS>" + averagedConsumptionsDataList.get(i).getSeconds() + "</SECONDS>");
+                    writer.println("\t</SIGNATURE>");
+                }
+                writer.println("</PETRA>");
+                writer.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            Notifications.Bus.notify(new Notification("IDNotifyCSV", "Success", "XML Downloaded Successfully.", NotificationType.INFORMATION));
+        }
+        else
+        {
+
         }
     }
 }
