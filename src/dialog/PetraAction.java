7125c97007a7cb6de7240afab917035b21ccfdf9
robtnr <roberto.cont94@gmail.com>
2017-10-10 01:34:35 +0200

    Risolti bug per la creazione della cartella "resources"

--- src/dialog/PetraAction.java
+++ src/dialog/PetraAction.java
@@ -2,6 +2,9 @@ package dialog;
 
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -22,24 +25,22 @@ public class PetraAction extends AnAction {
 
         final PluginId pluginId = PluginId.getId("IdPetra");
         final IdeaPluginDescriptor pluginDescriptor = PluginManager.getPlugin(pluginId);
-        //Creazione cartella "risorse"
+        //Create folder "resources"
         if(!new File(pluginDescriptor.getPath().getAbsolutePath()+"/resources").exists())
         {
             new File(pluginDescriptor.getPath().getAbsolutePath() + "/resources").mkdirs();
-            File result = new File(getClass().getClassLoader().getResource("/result.csv").getFile());
-            File apktool = new File(getClass().getClassLoader().getResource("/apktool_2.2.2.jar").getFile());
-            File monkey_playback = new File(getClass().getClassLoader().getResource("/monkey_playback.py").getFile());
-
             File dest_result = new File(pluginDescriptor.getPath().getAbsolutePath() + "/resources/result.csv");
             File apktool_result = new File(pluginDescriptor.getPath().getAbsolutePath() + "/resources/apktool_2.2.2.jar");
             File monkey_playback_result = new File(pluginDescriptor.getPath().getAbsolutePath() + "/resources/monkey_playback.py");
             try {
-                FileUtils.copyFile(result, dest_result);
-                FileUtils.copyFile(apktool, apktool_result);
-                FileUtils.copyFile(monkey_playback, monkey_playback_result);
+                FileUtils.copyURLToFile(getClass().getResource("/result.csv"),dest_result);
+                FileUtils.copyURLToFile(getClass().getResource("/apktool_2.2.2.jar"),apktool_result);
+                FileUtils.copyURLToFile(getClass().getResource("/monkey_playback.py"),monkey_playback_result);
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
+
+
 
         }
         new UIPetra(project).show();
