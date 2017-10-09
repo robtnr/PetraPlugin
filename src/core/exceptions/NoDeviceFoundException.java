d4aebb0e93f4d1250184f502813b328228dfaf62
robtnr <roberto.cont94@gmail.com>
2017-10-09 12:04:39 +0200

    Aggiunta la Notifications nell'exception

--- src/core/exceptions/NoDeviceFoundException.java
+++ src/core/exceptions/NoDeviceFoundException.java
@@ -1,5 +1,9 @@
 package core.exceptions;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+
 /**
  * @author dardin88
  */
@@ -8,5 +12,6 @@ public class NoDeviceFoundException extends Exception {
     public NoDeviceFoundException() {
         super("error: no device/emulator found!");
         System.out.println("error: no device/emulator found!");
+        Notifications.Bus.notify(new Notification("IDDeviceMissing", "Error", "no device/emulator found!", NotificationType.ERROR));
     }
 }
